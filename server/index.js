c239a5763c5b3d9a3bcefe4bb1df29751aa4f466
Trevor Waters <twater623@west-mec.org>
2024-10-02 13:37:46 -0700

    added a line

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
+const {check, validationResult} = require('express-validator');
 
 const app = express();
 
